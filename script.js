35e5bbbbfe277e25f9a04ef8994092002969981e
joaombvb <joaombvb@gmail.com>
2024-09-06 17:06:02 -0300

    atualiza classes e ids e inicia js

---
+++ script.js
@@ -0,0 +1,34 @@
+const item = document.getElementById("input-item");
+const botaoSalvarItem = document.getElementById("button-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+botaoSalvarItem.addEventListener("click", adicionarItem);
+
+function adicionarItem(evento) {
+    evento.preventDefault()
+
+    const itemDaLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("lista-item-container");
+
+    const containerNomeDoItem = document.createElement("div");
+    const nomeDoItem = document.createElement("p");
+    nomeDoItem.innerText = item.value;
+    containerNomeDoItem.appendChild(nomeDoItem);
+
+    const containerBotoes = document.createElement("div");
+    const botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("item-lista-button");
+
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src ="img/delete.svg";
+    imagemRemover.alt = "Remover";
+
+    botaoRemover.appendChild(imagemRemover);
+    containerBotoes.appendChild(botaoRemover);
+
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
+    itemDaLista.appendChild(containerItemLista);
+    listaDeCompras.appendChild(itemDaLista);
+}
